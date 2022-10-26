a846c5a4e8436d86bd97d0142de4eaf30e73ba3e
Louis BLESSE <louis.blesse@efrei.net>
2022-10-26 16:53:50 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Quel programme voulez vous lancer ? \n 1 : Nourrir les Pigeons \n 2 : Jeu d'échec");
+        int choix = myObj.nextInt();
+
+        if (choix == 1){
+            System.out.println("WIP");
+        }
+        else if (choix == 2){
+            System.out.println("WIP");
+        }
+    }
+}
